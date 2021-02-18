da26367a2f138e9786f6167d650cec9b3441f152
Uriah Book <812646@mystma.org>
2021-02-18 12:50:43 -0600

    Added Car Controller
    Added Car Controller

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public Rigidbody2D wheel_f, wheel_b;
+    public float power;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("d"))
+        {
+            wheel_f.AddTorque(-power);
+            wheel_b.AddTorque(-power);
+        }
+        if (Input.GetKey("a"))
+        {
+            wheel_f.AddTorque(power);
+            wheel_b.AddTorque(power);
+        }
+    }
+}
