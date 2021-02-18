f4b1bf55c4810cfe7009566b9afc187578c2d32d
Uriah Book <812646@mystma.org>
2021-02-18 14:44:37 -0600

    Setup Respawn
    Setup Respawn

--- Assets/Scripts/RespawnSystem.cs
+++ Assets/Scripts/RespawnSystem.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class RespawnSystem : MonoBehaviour
 {
+
+    GameObject body, wheelB, wheelF;
+    Vector3 startPos;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        body = GameObject.Find("Body");
+        wheelB = GameObject.Find("Wheel_B");
+        wheelF = GameObject.Find("Wheel_F");
+        startPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D c)
+    {
+        if (c.gameObject.tag == "Track")
+        {
+            body.transform.position = startPos;
+            body.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+            body.transform.rotation = Quaternion.identity;
+        }
     }
 }
